5c3467cd2b23c8cd59442b925402e4b7815e238d
Oscar Wales <oscarjwales@gmail.com>
2021-12-21 21:37:24 +0000

    Further modal work

--- themes/weldon/front-page.php
+++ themes/weldon/front-page.php
@@ -146,11 +146,15 @@ if ( have_posts() ) {
 								<?php endforeach;
 							else:
 								foreach($previous as $job): ?>
-									<div class="work__job <?= count($previous) == 2 ? 'pair' : ''; ?>">
+									<div class="work__job has_modal <?= count($previous) == 2 ? 'pair' : ''; ?>">
 										<div class="work__job-image">
 											<img class="b-lazy" src="<?= get_field('feature_image', $job)['url']; ?>" data-src="<?= get_field('feature_image', $job)['url']; ?>" alt="<?= get_field('feature_image', $job)['alt']; ?>" />
 										</div>
 										<div class="work__job-copy">
+											<h3><?= get_field('title', $job); ?></h3>
+										</div>
+										<div class="work__job-mask"></div>
+										<div class="work__job-modal">
 											<h3><?= get_field('title', $job); ?></h3>
 										</div>
 									</div>
